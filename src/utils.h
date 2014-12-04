a8d73d0cc2a7721d73a763aba7ddf6766608133d
cabelitos <cabelitos@uol.com.br>
2014-12-04 10:56:39 -0200

    Make it compile on Linux (Fedora)

--- src/utils.h
+++ src/utils.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <math.h>
 #include <stdbool.h>
 
 typedef enum _KEYS {
@@ -15,6 +16,12 @@ typedef struct _POINT {
   float x;
   float y;
 } POINT;
+
+#ifndef M_PI
+
+#define M_PI 3.14159265358979323846264338327
+
+#endif
 
 POINT rotate_point(POINT p, POINT center, float degrees);
 bool isInvisible(POINT p, POINT display);
